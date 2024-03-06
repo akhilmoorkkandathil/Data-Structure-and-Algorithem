d0c561a17a52483ea6a123b40b0cb6d181187938
akhilmoorkkandathil <akhildasxyz@gmail.com>
2024-03-06 07:32:23 +0530

    Linked list

--- do.js
+++ do.js
@@ -120,6 +120,184 @@
 //     }
 // }
 
+
+//Write a JavaScript program to create and display a Singly Linked List.
+// class Node {
+//     constructor(val){
+//         this.val = val;
+//          this.next = null
+//     }
+// }
+// class SinglyLinkedList{
+//     constructor(){
+//         this.head = null;
+//         this.length = 0;
+//     }
+    
+//     push(val){
+//         const node = new Node(val);
+//         if(!this.head){
+//             this.head = node;
+//         }else{
+//             let curr = this.head;
+//         while(curr.next){
+//             curr = curr.next;
+//         }
+//         curr.next = node;
+        
+//     }
+//     this.length++
+//     return true
+//     }
+
+//     print(){
+//         if(!this.head){
+//             console.log("List is empty");
+//         } else {
+//             let str = "";
+//             let curr = this.head;
+//             while(curr){
+//                 str += curr.val + " "; 
+//                 curr = curr.next;
+//             }
+//             console.log(str);
+//         }
+//     }
+// }
+
+// const list = new SinglyLinkedList()
+// list.print()
+// console.log(list.push(4));
+// list.push(1)
+// list.push(2)
+// list.print()
+
+// Write a JavaScript program to create a singly linked list of n nodes and display it in reverse order.
+// class Node {
+//     constructor(val){
+//         this.val = val;
+//          this.next = null
+//     }
+// }
+// class SinglyLinkedList{
+//     constructor(){
+//         this.head = null;
+//         this.length = 0;
+//     }
+    
+//     push(val){
+//         const node = new Node(val);
+//         if(!this.head){
+//             this.head = node;
+//         }else{
+//             let curr = this.head;
+//         while(curr.next){
+//             curr = curr.next;
+//         }
+//         curr.next = node;
+        
+//     }
+//     this.length++
+//     return true
+//     }
+//     reverse_list(){
+//         let prevNode = null;
+//         let currentNode = this.head;
+//         if(currentNode===null) return;
+    
+//         let nextNode;
+//         while(currentNode){
+//             nextNode = currentNode.next;
+//             currentNode.next = prevNode;
+//             prevNode = currentNode;
+//             currentNode = nextNode;
+//         }
+//         this.head = prevNode;
+//     }
+
+    
+//     displayList(){
+//         let node = this.head;
+//         var str = ""
+//         while (node) {
+//             str += node.val + "->";
+//             node = node.next;
+//         }
+//         str += "NULL"
+//         console.log(str);  
+//      } 
+// }
+
+// const list = new SinglyLinkedList()
+// console.log(list.push(4));
+// list.push(1)
+// list.push(2)
+// list.push(3)
+// list.displayList()
+// list.reverse_list()
+// list.displayList()
+
+//Write a JavaScript program to create a singly linked list of n nodes and count the number of nodes.
+// class Node {
+//     constructor(val){
+//         this.val = val;
+//          this.next = null
+//     }
+// }
+// class SinglyLinkedList{
+//     constructor(){
+//         this.head = null;
+//         this.length = 0;
+//     }
+    
+//     push(val){
+//         const node = new Node(val);
+//         if(!this.head){
+//             this.head = node;
+//         }else{
+//             let curr = this.head;
+//         while(curr.next){
+//             curr = curr.next;
+//         }
+//         curr.next = node;
+        
+//     }
+//     this.length++
+//     return true
+//     }
+    
+//     findCount(){
+//         let currNode = this.head;
+//         let  count = 0;
+//         while(currNode){
+//             count++;
+//             currNode = currNode.next;
+//         }
+//         return count
+//     }
+
+    
+//     displayList(){
+//         let node = this.head;
+//         var str = ""
+//         while (node) {
+//             str += node.val + "->";
+//             node = node.next;
+//         }
+//         str += "NULL"
+//         console.log(str);  
+//      } 
+// }
+
+// const list = new SinglyLinkedList()
+// // console.log(list.push(4));
+// // list.push(1)
+// // list.push(2)
+// // list.push(3)
+// // list.displayList()
+// console.log(list.findCount());
+
+//Write a JavaScript program to insert a node at any position in a Singly Linked List.
 class Node {
     constructor(val){
         this.val = val;
@@ -136,35 +314,166 @@ class SinglyLinkedList{
         const node = new Node(val);
         if(!this.head){
             this.head = node;
-            this.length++;
         }else{
-            const curr = this.head;
+            let curr = this.head;
         while(curr.next){
             curr = curr.next;
         }
         curr.next = node;
-        this.length++
         
     }
+    this.length++
     return true
     }
 
-    print(){
-        if(!this.head){
-            console.log("List is empty");
-        }
-        else{
-            let str = "";
-        let curr = this.head;
-        while(curr){
-            str+=curr.val;
-        }
-        console.log(str);
+    addBignning(val){
+        const newNode = new Node(val);
+        if(!this.head ){
+            this.head = newNode;
+            this.length++;
+        }else{
+            let ref = this.head;
+            this.head = newNode;
+            newNode.next = ref;
+            this.length++
+            return true;
+
         }
     }
+
+    addEnd(val){
+        const newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.length++
+        }else{
+            let currNode = this.head;
+            while(currNode.next){
+                currNode=currNode.next;
+            }
+            currNode.next = newNode;
+            this.length++
+            return true
+
+        }
+    }
+    
+    insert(index,val){
+        const newNode = new Node(val);
+        if(index>this.length || index<0) return false
+        let currNode = this.head;
+        let count = 1;
+        while(count !== index){
+            count++;
+            currNode = currNode.next;
+        }
+        let ref = currNode.next;
+        currNode.next = newNode
+        newNode.next = ref;
+        return true
+
+    }
+
+    getNode(index){
+        if(index>this.length || index<0) return false
+        let currNode = this.head;
+        let count =0;
+        while(count!=index){
+            currNode = currNode.next;
+        }
+        return currNode;
+    }
+
+    find(val){
+        if(!this.head) return false
+        let currNode = this.head;
+        while(currNode.val!==val){
+            currNode = currNode.next;
+            if(!currNode) return -1
+        }
+        return currNode
+    }
+
+    toArray(){
+        if(!this.head) return false;
+        let currNode = this.head;
+        let arr = [];
+        while(currNode){
+            arr.push(currNode.val);
+            currNode = currNode.next;
+        }
+        return arr;
+    }
+
+    findMiddle(){
+        if(!this.head) return null;
+        if(this.length==1) return this.head;
+        let fast = this.head;
+        let slow = this.head;
+        while(fast && fast.next){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    toLinkedList(arr){
+        if (arr.length == 0) return false
+        this.head = new Node(arr[0])
+        let currNode = this.head;
+        for(let i=1;i<arr.length;i++){
+            currNode.next = new Node(arr[i]);
+            currNode = currNode.next
+        }
+        return this
+    }
+
+    
+    displayList(){
+        let node = this.head;
+        var str = ""
+        while (node) {
+            str += node.val + "->";
+            node = node.next;
+        }
+        str += "NULL"
+        console.log(str);  
+     } 
 }
 
 const list = new SinglyLinkedList()
-list.print()
 console.log(list.push(4));
-list.print()
+list.push(1)
+list.push(2)
+list.push(3)
+list.push(5)
+
+list.displayList()
+list.insert(1,5)
+list.addBignning(10)
+list.addEnd(10)
+list.displayList();
+console.log(list.getNode(0));
+console.log(list.find(44));
+//console.log(list.toArray());
+console.log('=========');
+console.log(list.findMiddle());
+console.log(reverseList(list));
+
+//console.log(list.toLinkedList([2,3,4,5,6,67,7,8]));
+
+
+// class Node {
+//         constructor(val){
+//             this.val = val;
+//              this.next = null
+//         }
+//     }
+
+// const newNode = new Node(10);
+// const newNode2 = new Node(11);
+// newNode.next = newNode2;
+
+// console.log(newNode);
+
+
